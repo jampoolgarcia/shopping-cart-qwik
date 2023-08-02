7b45ead19714002c34c0976700579ff5017b24e5
jampoolg <jampoolgarcia@gmail.com>
2023-08-02 07:31:32 -0400

    product list request

--- src/components/product/product-item.tsx
+++ src/components/product/product-item.tsx
@@ -3,28 +3,32 @@ import { BsCartPlus } from '@qwikest/icons/bootstrap';
 
 import { IProduct } from '~/interface';
 
-export const ProductItem = component$(({ rating:  { count }, image, title, description,  price}: IProduct) => {
+interface Props {
+  product: IProduct
+}
+
+export const ProductItem = component$(({ product }: Props ) => {
   return (
     <>
         
         <div class="card flex flex-col items-end">
-            <span class="badge mr-3 mt-3 ml-auto">Cantidad: { count } </span>
+            <span class="badge mr-3 mt-3 ml-auto">Cantidad: { product.rating.count } </span>
             <img
-            src={image}
-            class="w-full"
+            src={product.image}
+            class="w-full max-h-[320px] min-h-[320px]"
+            width="320"
+            height="320"
             alt="imagen" 
-            width="200"
-             height="200"
             />
             <div class="px-6 py-4">
-                <h4 class="font-bold text-xl mb-2">{ title }</h4>
-                <p class="text-gray-700 text-base">{ description }</p>
+                <h4 class="font-bold text-xl mb-2 h-14 truncate-text-2">{ product.title }</h4>
+                <p class="text-gray-700 text-base truncate-text-3">{ product.description }</p>
             </div>  
-            <div class="w-full px-6 pt-4 pb-3 flex justify-between items-center">
+            <div class="w-full px-6 pb-3 flex justify-between items-center">
                 <button class="bg-blue-700 rounded p-2 text-white font-bold flex justify-center">
                   <span class="inline-block mr-1 text-xl"><BsCartPlus /></span>Agregar
                 </button>
-                <span class="bg-green-700 rounded-xl px-3 text-white text-center">{ price }$</span>
+                <span class="bg-green-700 rounded-xl px-3 text-white text-center">{ product.price }$</span>
             </div>
         </div>
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,42 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
 
-import { ProductList } from "~/components/product";
+import { ProductItem } from "~/components/product";
+import { getProducts } from "~/helpers/get-products";
+import { IProduct } from "~/interface";
+
+export const useProductList = routeLoader$<IProduct[]>(async() =>{
+  const products = await getProducts();
+  return products;
+})
 
 export default component$(() => {
-  return (
-    <>
+
+  const products = useProductList();
+
+  return (<>
       <div class="mt-14 flex flex-col items-center w-full">
 
-        <header class="bg-white w-full text-center py-6">
-          <h1 class="text-4xl">¡Bienvenido a vamos a comprar!</h1>
-          <p>
-            Estás a pocos clics de distancia para comprar las mejores cosas en el mercado en línea.
-          </p>
-        </header>
-        
+      <header class="bg-white w-full text-center py-6">
+        <h1 class="text-4xl">¡Bienvenido a vamos a comprar!</h1>
+        <p>
+          Estás a pocos clics de distancia para comprar las mejores cosas en el mercado en línea.
+        </p>
+      </header>
 
-        <ProductList />
+          <div class="my-5 mx-2 grid lg:grid-cols-3 md:grid-cols-2 xs:grid-clos-1 gap-4 lg:w-[80%]">
+              {
+                products.value.map((product) => (
+                  <div key={product.id}>
+                    <ProductItem product={product} />
+                  </div>
+                ))
+              }
+          </div>
       </div>
-      
-    </>
-  );
-});
+    </>)
+    
+    });
 
 export const head: DocumentHead = {
   title: "Bienvenido a Store",
